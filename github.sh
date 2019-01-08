0f2fd227cab6adbe88b987e8873d49427b31aae3
jonandoni <cifp.txurdinaga.jonandoni@gmail.com>
2019-01-08 10:39:56 +0100

    commit mar ene  8 10:39:56 CET 2019

--- github.sh
+++ github.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
+( echo "pantxisto" ; echo "CtM5010" ) | github.sh
 git add .
 fecha=`date`
 git commit -m "commit $fecha"
 git push
-echo 'pantxisto'
-echo 'pantxisto'
-echo 'pantxisto'
-echo 'pantxisto'
-echo 'pantxisto'
-echo 'pantxisto'
-echo 'pantxisto'
-echo 'pantxisto'
-echo 'pantxisto'
-echo 'pantxisto'
-echo 'pantxisto'
-echo 'pantxisto'
-echo 'pantxisto'
