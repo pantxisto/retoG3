474ad29eb549129936f1d1d45f5072d69ecf0212
jonandoni <cifp.txurdinaga.jonandoni@gmail.com>
2019-01-08 12:01:38 +0100

    commit mar ene  8 12:01:38 CET 2019

--- CrearCopiaDeSeguridad.sh
+++ CrearCopiaDeSeguridad.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-git add .
-fecha=`date`
-git commit -m "commit $fecha"
-git push
 echo -n "¿Hacer git pull? s/n: "
 read respuesta
 if [[ $respuesta = "s" ]]
 then
     git pull
 fi
+git add .
+fecha=`date`
+git commit -m "commit $fecha"
+git push
