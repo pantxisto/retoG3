e6e22e79434adc034bc3463db77ef2b5f2fbc849
jonandoni <cifp.txurdinaga.jonandoni@gmail.com>
2019-01-08 10:49:40 +0100

    commit mar ene  8 10:49:40 CET 2019

--- github.sh
+++ github.sh
@@ -3,6 +3,7 @@ git add .
 fecha=`date`
 git commit -m "commit $fecha"
 echo "Introduce tu nombre"
-read prueba | echo "jon"
+read prueba | echo "jon" | echo -ne '\n'
+
 echo "pantxisto"
 # echo "pantxisto" | git push
