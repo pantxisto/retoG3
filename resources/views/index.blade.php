3cdc86711a3301bd9eea429f083b5e4098ae60d6
j0s3.90jr@gmail.com <jonandonicastelo@outlook.com>
2019-01-09 09:58:10 +0100

    09-01-19 plantilla

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,5 +5,31 @@
 @endsection
 
 @section('contenido')
-    <p>Este apartado aparecerá en la sección "content".</p>
+    <section>
+        <article>
+            <!--
+            <form action="">
+                <fieldset>
+                    <legend><h2>Login</h2></legend>
+                    <label for="email">Email: </label><input type="email" name="email" id="" placeholder="email@mail.com"><br>
+                    <label for="pass">Contraseña: </label><input type="password" name="pass" placeholder="8-16 caracteres"><br>
+                    <button>Entrar</button>
+                </fieldset>
+            </form>
+            -->
+            <h3>Ultimas ofertas</h3>
+            <div class='oferta'>
+                    <h5>Programador Web</h5>
+                    <time>10/12/2018</time><br>
+                    <p>Empresa: Ibermatica <br>
+                        Localidad: Bilbao</p>
+            </div>
+            <div class='oferta'>
+                    <h5>Programador Multiplataforma</h5>
+                    <time>10/12/2018</time><br>
+                    <p>Empresa: Accentur <br>
+                        Localidad: Bilbao</p>
+            </div>
+        </article>
+    </section>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,9 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Maqueta</title>
+        <title>Inicio</title>
         <link rel="stylesheet" href="assets/css/bootstrap/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/estilo.css">
+        <link rel="stylesheet" href="assets/css/estilo.css">
+
+        <link rel="icon" href="img/favicon.ico" type="image/x-icon">
     </head>
 <body>
     <div class="container">
@@ -26,11 +28,11 @@
                 <nav class="col-lg-12 nav">
 
                 @if( true || Auth::check() )
-                    <ul class="col-12 nav d-flex">
-                        <li class="col-2 dropdown item item" id='inicio'>Inicio</li>
-                        <li class="col-2 dropdown item item" id='ofertas'>Ofertas</li>
-                        <li class="col-2 dropdown item item" id='administrar'>Administrar</li>
-                        <li class="col-2 dropdown item item" id='perfil'>Perfil</li>
+                    <ul class="col-12 nav d-flex font-weight-bold text-white text-center">
+                        <li class="col-lg-2 col-sm-12 dropdown item" id='inicio'>Inicio</li>
+                        <li class="col-lg-2 col-sm-12 dropdown item" id='ofertas'>Ofertas</li>
+                        <li class="col-lg-2 col-sm-12 dropdown item" id='administrar'>Administrar</li>
+                        <li class="col-lg-2 col-sm-12 dropdown item" id='perfil'>Perfil</li>
                     </ul>
                 @endif
 
@@ -43,12 +45,16 @@
         </div>
 
         @section('footer')
-            <footer>
-                <p>CIFP Txurdinaga LHII <br>
-                    Teléfono: 944125712 <br>
-                    Fax: 944731359 <br>
-                    idazkaria@fpTXurdinaga.com</p>
-            </footer>
+                <div class="row d-flex justify-content-between bg-dark text-white">
+                    <div class="m-2">
+                        CIFP Txurdinaga LHII <br>
+                    </div>
+                    <div class="m-2">
+                        Teléfono: 944125712 <br>
+                        Fax: 944731359 <br>
+                        idazkaria@fpTXurdinaga.com
+                    </div>
+                </div>
         @show
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
