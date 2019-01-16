f2847db61f96f95312818b9f9b24792e318f3000
j0tace <j0s3.90jr@gmail.com>
2019-01-16 09:15:39 +0100

    creacion de controllers, models

---
+++ database/seeds/EmpresasSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EmpresasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        //for ($i=0; $i < 50; $i++) {
+		DB::table('empresas')->insert([
+           'nomEmpresa' => 'Avant Group',
+           'sector'  => 'TIC',
+           'email'  => 'info@avangroup.com',
+           'ubicacion'  => 'Zamudio (Bilbao)',
+           'telefono'  => '(+34) 946567700',
+           'link'  => 'www.avangroup.com',
+           'fecAlta'  => date('Y-m-d H:m:s')
+   		]);
+    	//));
+
+			DB::table('empresas')->insert([
+           'nomEmpresa' => 'Cisco',
+           'sector'  => 'TIC',
+           'email'  => '',
+           'ubicacion'  => 'Zamudio (Bilbao)',
+           'telefono'  => '(+34) 944310200',
+           'link'  => 'www.cisco.com',
+           'fecAlta'  => date('Y-m-d H:m:s')
+       ]);
+
+			DB::table('empresas')->insert([
+           'nomEmpresa' => 'Ibermaticap',
+           'sector'  => 'TIC',
+           'email'  => '',
+           'ubicacion'  => 'Zamudio (Bilbao)',
+           'telefono'  => '(+34) 946567700',
+           'link'  => 'www.ibermatica.com',
+           'fecAlta'  => date('Y-m-d H:m:s')
+       ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,8 @@ Route::get('/empresas', function () {
     return view('empresas');
 });
 
+Route::get('user/{id}', 'UserController@showProfile');
+
 Route::get('/perfil', function () {
     return view('perfil');
 });
