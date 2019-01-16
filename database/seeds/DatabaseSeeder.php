1cf6f0f6c02ab7d649c55a774d2357d3eece1c65
j0tace <j0s3.90jr@gmail.com>
2019-01-16 09:41:32 +0100

    Creacion de datos mediantes seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        //$this->call(EmpresasSeeder::class);
-        //$this->call(DepartamentosSeeder::class);
-        //$this->call(CurriculumsSeeder::class);
-        //$this->call(GradosSeeder::class);
-        //$this->call(OfertaSolicitadaSeeder::class);
-        //$this->call(OfertasSeeder::class);
-        //$this->call(TituloGradosSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        $this->call(EmpresasSeeder::class);
+        $this->call(DepartamentosSeeder::class);
+        $this->call(CurriculumsSeeder::class);
+        $this->call(GradosSeeder::class);
+        $this->call(OfertaSolicitadaSeeder::class);
+        $this->call(OfertasSeeder::class);
+        $this->call(TituloGradosSeeder::class);
         $this->call(UsuariosSeeder::class);
     }
 }
