aba7ef6535675eb719d8450796c7dd03c93b2e17
j0s3.90jr@gmail.com <jonandonicastelo@outlook.com>
2019-01-21 12:24:54 +0100

    partial ofertas

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('auth.login');
+});*/
+Route::get('/', function () {
+    return view('index');
 });
 
 
