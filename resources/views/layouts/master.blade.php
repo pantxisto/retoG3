6550d154b3ba8a1365edb81e11df9460746d3849
1INav <ander.itza@txorierri.com>
2019-01-22 13:30:12 +0100

    22-01-2019 Unai

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,6 @@
         <title>Inicio</title>
         <link rel="stylesheet" href="assets/css/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/estilo.css">
-
         <link rel="icon" href="img/favicon.ico" type="image/x-icon">
     </head>
 <body>
@@ -31,7 +30,8 @@
                     <a href="http://www.hezkuntza.ejgv.euskadi.eus/r43-2591/eu"><img src="img/gobiernoVasco.png" alt=""></a>
             </div>
         </div>
-        
+
+        <script src="{{asset('js/app.js')}}"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
--- resources/views/ofertas.blade.php
+++ resources/views/ofertas.blade.php
@@ -1,54 +1,31 @@
 @extends('layouts.master')
 
 @section('contenido')
-<div class=''>
-    <section>
-        <article>
-            <h3 class='text-center'>Ultimas ofertas</h3>
-            <div class='oferta'>
-                <h5>Programador Web</h5>
-                <time>10/12/2018</time><br>
-                <p>Empresa: Ibermatica <br>
-                    Localidad: Bilbao</p>
+    <div class="contenido-ofertas">
 
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Detalles</button>
+            <div class="filtros">
+                <div class="form-check">
+                    <h6><b>Ordenar por:</b></h6>
+                        <input type="checkbox" class="form-check-input" id="materialUnchecked">
+                        <label class="form-check-label" for="materialUnchecked">Fecha de publicacion</label><br>
+                        <input type="checkbox" class="form-check-input" id="materialUnchecked">
+                        <label class="form-check-label" for="materialUnchecked">Ultimas 24h</label><br>
+                        <input type="checkbox" class="form-check-input" id="materialUnchecked">
+                        <label class="form-check-label" for="materialUnchecked">Ultimos 7 dias</label><br>
+                        <input type="checkbox" class="form-check-input" id="materialUnchecked">
+                        <label class="form-check-label" for="materialUnchecked">Ultimos 15 dias</label><br><br>
+                    <h6><b>Puestos:</b></h6>
+                        <input type="checkbox" class="form-check-input" id="materialUnchecked">
+                        <label class="form-check-label" for="materialUnchecked">Desarrollo web</label><br>
+                        <input type="checkbox" class="form-check-input" id="materialUnchecked">
+                        <label class="form-check-label" for="materialUnchecked">Multiplataforma</label><br>
+                        <input type="checkbox" class="form-check-input" id="materialUnchecked">
+                        <label class="form-check-label" for="materialUnchecked">Otro</label><br>
+                        <input type="checkbox" class="form-check-input" id="materialUnchecked">
+                        <label class="form-check-label" for="materialUnchecked">Otros</label><br>
                 
-                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <form>
-                        <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">Recipient:</label>
-                            <input type="text" class="form-control" id="recipient-name">
-                        </div>
-                        <div class="form-group">
-                            <label for="message-text" class="col-form-label">Message:</label>
-                            <textarea class="form-control" id="message-text"></textarea>
-                        </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Send message</button>
-                    </div>
-                    </div>
-                </div>
-                </div>
             </div>
-            <div class='oferta'>
-                <h5>Programador Multiplataforma</h5>
-                <time>10/12/2018</time><br>
-                <p>Empresa: Accentur <br>
-                    Localidad: Bilbao</p>
-            </div>
-        </article>
-    </section>
-</div>
+        </div>
+        @include('partials.ofertas')
+    </div>  
 @stop
