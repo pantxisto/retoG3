e5c2d57a76ffe91ee0401b9061561b6565740458
Guaperas <julenlodeiro@outlook.com>
2019-01-24 13:13:07 +0100

    Parte empresas diseñada

--- app/Http/Controllers/EmpresasController.php
+++ app/Http/Controllers/EmpresasController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Empresa;
 
 class EmpresasController extends Controller
 {
-    //
+    public function index(Request $request){
+        $empresas = Empresa::all();
+        return view('empresas',['empresas'=>$empresas]);
+    }
 }
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,4 @@
-<?php 
-    function logout(){
-        Auth::logout(); 
-        return redirect()->route(url('login')); 
-    }
-?>
+
 <div class="d-flex justify-content-between align-items-center border-box">
     <a class="" href="http://www.fptxurdinaga.hezkuntza.net/web/Guest"><img  src="img/logo.png" alt="logo_centro"></a>
     <img class="img-fluid d-lg-block d-sm-none" src="img/texto.png" alt="">
@@ -11,25 +6,18 @@
     <div class="row">
         <button id='contacto' type='button'>Contacto</button>
 
-        @if(Auth::user()!==null)
             <button id='contacto' type='button' onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Desconectar</button>
-        @endif
+     
 
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
-
-        @if( Auth::user() !== null )
             <button class="dropdown-toggle" type="button" id="contacto" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {{ Auth::user()->name }}
+            
             </button>
             <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                 <a class="dropdown-item" href="#">Opción 1</a>
                 <a class="dropdown-item" href="#">Opción 2</a>
                 <a class="dropdown-item" href="#">Opción 3</a>
             </div>
-        @else
-        @endif
+     
     </div>
 
 </div>
